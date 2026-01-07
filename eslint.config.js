1d0ce5deba1bb7e957fe038be053c662a8ce127d
mobin20004b-lab <mobin.20004b@gmail.com>
2026-01-07 08:49:15 +0000

    Add flat ESLint configuration

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    files: ['**/*.ts'],
+    ignores: ['dist/**', 'node_modules/**'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', caughtErrors: 'none' },
+      ],
+    },
+  },
+];
